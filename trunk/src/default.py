eeef8e04d720d7b088ec43b1b1f46b0eb9bc3360
the.mobix <the.mobix@982a14aa-e176-11de-9097-f71ab900b11a>
2009-12-07 09:25:41 +0000

    Moved scripts to src folder in trunck. Many changes to script structure.

--- trunk/src/default.py
+++ trunk/src/default.py
@@ -1,15 +1,13 @@
 import appuifw, e32, thread, btsocket
 
 # Only for local tests
-import sys
-sys.path.append('E:\\download\\libs\\')
+#import sys
+#sys.path.append('E:\\download\\libs\\')
 
 import ftpserver
 
 class sypftp(object):
   def __init__(self):
-    self.uiConsole = appuifw.Text()
-    
     # Set look and feel
     appuifw.app.title = u"sypFTP"
     appuifw.app.menu = [
@@ -18,7 +16,7 @@ class sypftp(object):
       (u"About",      self.fakeBind),
       (u"Exit",       self.exit)
     ]
-    appuifw.app.body = self.uiConsole
+    appuifw.app.body = None
     
     # Bind "Exit" key
     appuifw.app.exit_key_handler = self.exit
@@ -28,7 +26,7 @@ class sypftp(object):
     
     # ftpserver init
     if self.network:
-      self.ftp_deamon()
+      self.ftp_server_deamon()
     
     # Lock and load (wait)
     self.app_lock = e32.Ao_lock()
@@ -39,7 +37,7 @@ class sypftp(object):
   
   def log(self, text):
     if text:
-      self.uiConsole.add(u"%s\n" % (text))
+      print u"%s" % (text)
   
   def networking(self):
     
@@ -58,31 +56,30 @@ class sypftp(object):
       self.log("failed.")
   
   def ftp_server_init(self):
-    self.authorizer   = ftpserver.DummyAuthorizer()
-    self.ftp_handler  = ftpserver.FTPHandler
+    self.ftp_authorizer   = ftpserver.DummyAuthorizer()
+    self.ftp_handler      = ftpserver.FTPHandler
     
-    self.authorizer.add_user('user', '12345', "E:\\", perm='elradfmw')
+    # Just for now, while everything is breaking ...
+    self.ftp_authorizer.add_user('user', '12345', "E:\\", perm='elradfmw')
     
-    self.ftp_handler.authorizer = self.authorizer
+    self.ftp_handler.authorizer = self.ftp_authorizer
     self.ftp_handler.banner     = "sypFTP"
     
-    self.address = (self.apo.ip(), 21)
-    self.ftpd = ftpserver.FTPServer(self.address, self.ftp_handler)
+    self.ftp_address = (self.apo.ip(), 121)
+    self.ftpd = ftpserver.FTPServer(self.ftp_address, self.ftp_handler)
     
     self.ftpd.max_cons = 256
     self.ftpd.max_cons_per_ip = 5
     
     self.ftpd.serve_forever()
   
-  def ftp_deamon(self):
-    self.lock = thread.allocate_lock()
-    tid = thread.start_new_thread(self.ftp_server_init, ())
-    appuifw.note(u"Thead no " + str(tid) + " is running.", "info")
+  def ftp_server_deamon(self):
+    self.thread_lock  = thread.allocate_lock()
+    self.ftp_thread   = thread.start_new_thread(self.ftp_server_init, ())
    
   def exit(self):
-    #self.apo.stop()
+    self.apo.stop()
     self.app_lock.signal()
 
 if __name__ == '__main__':
   sypftp()
-  
